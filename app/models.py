60c73b4e9e4a3e1516b81fbfe01244bd31bd92c3
Moxikai <zhu-hero@qq.com>
2016-09-23 09:04:50 +0800

    commit

--- app/models.py
+++ app/models.py
@@ -20,6 +20,54 @@ class Law(db.Model):
     status = db.Column(db.String(48))
     content = db.Column(db.Text)
 
+
+class Company(db.Model):
+    __tablename__ = 'company'
+
+    id = db.Column(db.String(48),primary_key=True)
+    companyName = db.Column(db.String(48))
+    legalRepresentative = db.Column(db.String(48))
+    phone400 = db.Column(db.String(48))
+    phone = db.Column(db.String(48))
+    fax = db.Column(db.String(48))
+    email = db.Column(db.String(48))
+    note = db.Column(db.Text)
+
+
+class Person(db.Model):
+    __tablename__='person'
+
+    id = db.Column(db.String(48),primary_key=True)
+    name = db.Column(db.String(48))
+    curriculumVitae = db.Column(db.Text)
+    platform_id = db.Column(db.String(48),db.ForeignKey('platform.id'))
+
+
+
+class Platform(db.Model):
+    __tablename__ = 'platform'
+
+    id = db.Column(db.String(48),primary_key=True)
+    name = db.Column(db.String(48))
+    gradeFromThird = db.Column(db.String(48))
+    profitAverage = db.Column(db.String(48))
+    dateSale = db.Column(db.String(48))
+    registeredCapital = db.Column(db.String(48))
+    area = db.Column(db.String(48))
+    url = db.Column(db.String(48))
+    startMoney = db.Column(db.String(48))
+    managementFee = db.Column(db.String(48))
+    cashTakingFee = db.Column(db.String(48))
+    backGround = db.Column(db.String(48))
+    provisionOfRisk = db.Column(db.String(48))
+    foundCustodian = db.Column(db.String(48))
+    safeguardWay = db.Column(db.String(48))
+    assignmentOfDebt = db.Column(db.String(48))
+    automaticBidding = db.Column(db.String(48))
+    cashTime = db.Column(db.String(48))
+    persons = db.relationship('Person',backref='platform')
+
+
 if __name__ == '__main__':
     pass
     db.create_all()
